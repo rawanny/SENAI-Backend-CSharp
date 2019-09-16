5fad3b599db55396548af1342a0b01f13f234123
FIC <FIC@info.local>
2019-09-16 15:55:49 -0300

    Exercicios aula 2

---
+++ Exercicios/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Exercicio_1
+    {
+        static void Main(string[] args)
+        {
+            float resultado, salario, despesa;
+            String retorno;
+
+            Console.WriteLine("Digite seu salário");
+            salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite suas despesas");
+            despesa = float.Parse(Console.ReadLine());
+
+            resultado = salario - despesa;
+
+            if(resultado > 0){
+                retorno = "Superávit";
+            }else{
+                retorno = "Déficit";
+            }
+            Console.WriteLine("Seu saldo é " + retorno);
+            
+        }
+    }
+}
